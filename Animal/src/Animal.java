1dd281276ea4e6e453d07dd8ee25befaaa6693ce
sushilpok <namrata.pokhrel2014@gmail.com>
2015-01-19 12:47:28 +0545

    Java Project

---
+++ Animal/src/Animal.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Sushil on 15/01/2015.
+ */
+public class Animal {
+    public static void main(String[] args) {
+
+        System.out.println("             '__'");
+        System.out.println("            (\u00A9 \u00A9)");
+        System.out.println("   /=========\\/");
+        System.out.println(" /  ||  %%  ||");
+        System.out.println("*   ||----  ||");
+        System.out.println("    \u00A5\u00A5      \u00A5\u00A5  ");
+        System.out.println("    \"\"     \"\"");
+    }
+}
